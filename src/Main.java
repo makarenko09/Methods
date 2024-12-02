096297dd37071686b5301896f81d4c4d51515bd2
avare41lll <konmakarenko@outlook.com>
2024-12-02 10:32:38 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("task 1");
+        // Реализуйте метод, который получает в качестве параметра год, проверяет, является ли он високосным, и выводит результат в консоль.
+        //
+        //Эту проверку вы уже реализовывали в задании по условным операторам.
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
+        System.out.println("End task 4");
+        //task 5
+        for (int i = 1904; i < 2096; i = i + 4) {
+            System.out.println(i + " год является високосным");
+        }
+
+    }
+}
